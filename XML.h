e50b370a1817b1ad7706979c0986f3bf79fdbb51
GehadHisham20 <73800429+GehadHisham20@users.noreply.github.com>
2021-07-13 22:08:43 +0200

    Create XML.h

---
+++ XML.h
@@ -0,0 +1,55 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QFile>
+#include <QTextStream>
+#include <QFileDialog>
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <stack>
+#include <sstream>
+#include <QtDebug>
+#include <QProcess>
+#include<QMessageBox>
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private slots:
+    void on_OpenFileButton_clicked();
+
+    void on_Prettify_Button_clicked();
+
+    void on_Save_Button_clicked();
+
+    void on_JSON_Button_clicked();
+
+    void on_Remove_Spaces_clicked();
+
+    void on_Check_Button_clicked();
+
+    void on_Reset_button_clicked();
+
+    void on_Exit_Button_clicked();
+
+    void on_Correct_Button_clicked();
+
+
+
+private:
+    Ui::MainWindow *ui;
+    
+};
+#endif // MAINWINDOW_H
